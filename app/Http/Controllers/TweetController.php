f86842381101bd272f974f7d6e99325320a93e32
Derpsantos <38963819+Derpsantos@users.noreply.github.com>
2019-02-13 23:15:49 -0500

    Added a tweet controller #7

---
+++ app/Http/Controllers/TweetController.php
@@ -0,0 +1,17 @@
+<?php
+
+ namespace App\Http\Controllers;
+
+ use Illuminate\Http\Request;
+
+ class TweetController extends Controller
+
+ {
+   /*     Database for likes needs to be created
+   public function likePost($id){
+     $likePost = DB::table('likes')->insert([
+     'post_id' => $id  
+     ]);
+   }
+   */
+ }
