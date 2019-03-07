84c7745c8e57f1bd490e78a83c2e67b77ae8e97c
Vinita Singh <vinita.singh1319@gmail.com>
2019-03-06 21:16:13 -0500

    Updated view feed front end

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-        <h1>Here is your feed!!</h1> 
+        <h1>Your Feed</h1>
 
 
 @foreach ($f_id as $fid)
 @foreach ($names as $nme)
 @if($nme->id == $fid->follow_id)
 <td>
-<h2><div>username: {{ $nme->name }}</div></h2>
+<hr>
+<h5 style="color:blue"><div>@ {{ $nme->name }}</div></h5>
 </td>
       @foreach ($tweets as $tweet)
       @if($tweet->user_id == $fid->follow_id)
@@ -22,10 +23,11 @@
            <tr>
                 <td>
                 <a id="like" href="/like/{{$tweet->id}}" class="btn btn-default" data-tweetID="{{ $tweet->id }}">
-                            {{ $tweet->like_cnt }} Like 
+                            {{ $tweet->like_cnt }} Like
                 </a>
                 </td>
             </tr>
+            <hr>
            <thead>
               <h6><small>{{$tweet->time_psosted}}</small></h6>
             </thead>
