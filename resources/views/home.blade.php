4998d7fe0f5a67b05ebef19346391949beb08008
kasrarad <kasral89@gmail.com>
2019-03-17 13:35:40 -0400

    corrected version related to issues #87 #89

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,13 @@
                 <br>
                 <br>
               <div class="panel panel-default"> 
-               <div class="panel-heading"> Home <span class="pull-right"><a href="/listings/create" class="btn btn-success btn-xs">profile</a></span>
+                @if(count($listings))
+                    @foreach($listings as $listing)
+                        <div class="panel-heading"> Home <span class="pull-right"><a href="/listings/{{$listing->id}}/edit" class="btn btn-success btn-xs">profile</a></span>
+                    @endforeach
+                @else
+                    <div class="panel-heading"> Home <span class="pull-right"><a href="/listings/create" class="btn btn-success btn-xs">profile</a></span>
+                @endif   
                     <div class="panel-body">
                         @if(count($listings))
                             <table class="table table-striped">
@@ -55,7 +61,6 @@
                                         <td>Website: {{$listing->website}}</td>
                                     </tr>
                                     <tr>
-                                        <td><a class="pull-right btn btn-default" href="/listings/{{$listing->id}}/edit">Edit</a></td> 
                                         <td>
                                             {!!Form::open(['action' => ['ListingsController@destroy', $listing->id], 'method' => 'POST', 'class' => 'pull-left', 'onsubmit' => 'return confirm("Are you sure?")'])!!}
                                               {{Form::hidden('_method', 'DELETE')}}
